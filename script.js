567d602a25459b485088d5a79dd81f1744f42de2
xeo <mumulti03@gmail.com>
2026-09-12 20:20:07 +0500

    added yandex metric code

--- script.js
+++ script.js
@@ -19,12 +19,3 @@ closeBtn.addEventListener('click', closeLightbox);
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') closeLightbox();
 });
-
-
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-ym(112525756, "init", {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
